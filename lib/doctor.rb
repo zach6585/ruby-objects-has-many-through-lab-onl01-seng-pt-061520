e5037049493b3d3c6b9c5a9c8a41a9a895b153a6
zachary mines <zmines@wesleyan.edu>
2020-07-21 21:31:41 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor 
+  
+  @@all = []
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def appointments 
+    Appointment.all.select{|appointment| appointment.doctor == self}
+  end 
+  
+  def new_appointment(name, patient)
+    Appointment.new(name, self, patient)
+  end 
+  
+  def patients 
+    b = []
+    a = appointments 
+    a.each do |appointment|
+      b << appointment.patient 
+    end 
+    b
+  end 
+end 
